af0c518f464a73948cea3a13b7192e4b99f2d76f
Smalley <a01487@prod.norsk-tipping.no>
2016-06-02 15:43:46 +0200

    centering stuff added

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
-    
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Leicester'>Leicester, UK</Link>
+        </li>
+        <li>
+          <Link to='/?location=rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
+    </div>
   )
 };
 
